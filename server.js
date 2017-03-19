30bb2d1dcd8be47fcfb85c1108e879fe3f59ee35
albertoziveri <albertoziveri@me.com>
2017-03-19 18:14:00 +0000

    35

--- server.js
+++ server.js
@@ -220,6 +220,7 @@ slapp.message('attachment', ['mention', 'direct_message'], (msg) => {
 	    // QPX REST API URL (I censored my api key)
 	    var url = "https://api.fattureincloud.it:443/v1/fatture/nuovo"
 		var resoconto = {}
+		var response ="";
 		
 	    // fire request
 	    request({
@@ -229,9 +230,7 @@ slapp.message('attachment', ['mention', 'direct_message'], (msg) => {
 	}, function (error, response, body) {
 	        if (!error && response.statusCode === 200) {
 	            console.log(body)
-	            var response = JSON.parse(body);
-	            resoconto["doc_id"] = response.new_id;
-	            resoconto["email"] = invoiceData["indirizzo_via"];
+	            response = JSON.parse(body);
 	        }
 	        else {
 				msg.say("Fattura non inserita, procedere manualmente.")
@@ -241,6 +240,11 @@ slapp.message('attachment', ['mention', 'direct_message'], (msg) => {
 	            return
 	        }
 	    })
+	    
+	    resoconto["doc_id"] = response.new_id;
+	    resoconto["email"] = invoiceData["indirizzo_via"][0];
+	    msg.say(resoconto["doc_id"]);
+	    msg.say(resoconto["email"]);
 	    
 		msg
 		.say({

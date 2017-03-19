ec3bfbe677f16af68bd1be9b5af57bf6f4ba3c67
albertoziveri <albertoziveri@me.com>
2017-03-19 16:02:15 +0000

    32

--- server.js
+++ server.js
@@ -198,7 +198,7 @@ slapp.message('attachment', ['mention', 'direct_message'], (msg) => {
 	slapp.route('prezzo_prodotto', (msg,invoiceData) => {
 	  var response = (msg.body.event && msg.body.event.text) || ''
 	  response = response.replace(/,/g, '.');
-	  var price = Double.parseDouble(response);
+	  double price = Double.parseDouble(response);
 	  invoiceData["lista_articoli"][0]["prezzo_lordo"] = price;
 	  msg.say("Perfetto! Quindi hai venduto "+invoiceData["lista_articoli"][0]["nome"]+" unità di "+invoiceData["lista_articoli"][0]["quantita"]+" al prezzo di "+invoiceData["lista_articoli"][0]["prezzo_lordo"]+" "+invoiceData["valuta"])
 	  

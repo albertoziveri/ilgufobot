fd6cbfb0697c6e78162abb5d6c619b643a93bff1
albertoziveri <albertoziveri@me.com>
2017-03-20 17:57:13 +0000

    232323

--- server.js
+++ server.js
@@ -155,7 +155,7 @@ slapp.message('attachment', ['mention', 'direct_message'], (msg) => {
 	
 	slapp.route('articolo', (msg,invoiceData) => {
 	  var response = (msg.body.event && msg.body.event.text) || ''
-	  var realEmail = response.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/ig);
+	  var realEmail = response.match(/[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+/);
 	  console.log(realEmail);
 	  invoiceData["indirizzo_via"] = realEmail.join('\n');
 	  
